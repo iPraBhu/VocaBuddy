a4d4d385f7716f78aeba1c1ea25530cb7e8f57ac
unknown <pratik.bhuite@gmail.com>
2020-11-01 01:26:38 -0700

    initial commit

---
+++ vocabuddy-service/src/main/java/com/adevguide/vocabuddy/VocabuddyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adevguide.vocabuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabuddyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabuddyServiceApplication.class, args);
+	}
+
+}
